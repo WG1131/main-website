56bba263109f9409347c57f87d35650ae31b61d3
Johannes Zumthurm <nimro27@gmx.net>
2023-08-13 22:41:47 +0200

    fetch data from api

--- app/whoishome/page.tsx
+++ app/whoishome/page.tsx
@@ -1,18 +1,29 @@
 import Header from "../components/Header"
 
-export default function Home () {
+export interface Device {
+    name: string
+}
+
+async function getData() {
+    const res = await fetch('http://localhost:5000/whoishome', {cache: 'no-cache'})
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+
+}
+
+export default async function Home () {
+    const data: Device[] = await getData()
+
     return(
         <>
         <Header/>
         <p className="text-4xl text-center pt-20 pb-5">Who's home?</p>
         <div className="flex justify-center gap-5">
-            <div>Horacio</div>
-            <div>Lenja</div>
-            <div>Johannes</div>
-            <div>Elin</div>
-            <div>Skye</div>
-
-
+            {data.map((device) => <div>{device.name}</div>)}
         </div>
         </>
     )
