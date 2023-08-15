06bb13c76f5bcb10c3bd969249eaeb510e404c75
Johannes Zumthurm <nimro27@gmx.net>
2023-08-15 09:08:22 +0200

    update types whoishome

--- app/whoishome/page.tsx
+++ app/whoishome/page.tsx
@@ -4,27 +4,37 @@ export interface Device {
     name: string
 }
 
-async function getData() {
-    const res = await fetch('http://localhost:5000/whoishome', {cache: 'no-cache'})
-
+async function getWhoIsHome() {
+    const res = await fetch('http://localhost:5000/whoishome', { cache: 'no-cache' })
     if (!res.ok) {
-      throw new Error('Failed to fetch data')
+        return null
     }
-
     return res.json()
-
 }
 
-export default async function Home () {
-    const data: Device[] = await getData()
+const WhoIsHome = async () => {
+    const data: Device[] | null = await getWhoIsHome()
+    if (data) {
+        return (
+            <>
+                <div className="flex justify-center gap-5">
+                    {data.map((device) => <div key={device.name}>{device.name}</div>)}
+                </div>
+            </>
+        )
+    }
+    return (
+        <div className="flex justify-center">Data could not be loaded...</div>
+    )
+}
 
-    return(
+export default function Home() {
+
+    return (
         <>
-        <Header/>
-        <p className="text-4xl text-center pt-20 pb-5">Who's home?</p>
-        <div className="flex justify-center gap-5">
-            {data.map((device) => <div>{device.name}</div>)}
-        </div>
+            <Header />
+            <p className="text-4xl text-center pt-20 pb-5">Who's home?</p>
+            <WhoIsHome />
         </>
     )
 }
